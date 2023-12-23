79ae95f780e40bc773ff51c82783637ef50d5d3b
Endericedragon <fuze1007@163.com>
2023-12-23 17:26:08 +0800

    正在尝试进行依赖问题排查。 所有存在依赖问题的crate都被加上了# todo: error注释。

--- user/src/bin/my-program.rs
+++ user/src/bin/my-program.rs
@@ -4,11 +4,9 @@
 #[macro_use]
 extern crate user_lib;
 extern crate alloc;
-extern crate parity_scale_codec;
 
 use alloc::string::{String, ToString};
-use parity_scale_codec::{Encode, Decode};
-use parity_scale_codec_derive::*;
+use codec::{Encode, Decode};
 
 #[derive(Debug, PartialEq, Encode, Decode)]
 struct Test(u8);
@@ -21,7 +19,10 @@ pub fn main() -> i32 {
         println!("{:?}", slice);
         String::from_utf8_lossy(slice).into_owned()
     });
-    
-    println!("{:?}", a_encoded);
+
+    println!("{}", a_encoded);
+
+    let b = Test::decode(&mut a_encoded.as_bytes()).expect("Decode failed");
+    println!("{:?}", b);
     0
 }
